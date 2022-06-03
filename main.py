c9fcc055696dad814f18e4248337e3a2c02d3fcd
medh01 <mohamed.heni@insat.ucar.tn>
2022-06-03 14:36:16 +0000

    close to finishing the game

--- main.py
+++ main.py
@@ -6,119 +6,121 @@ pg.init()
 screen = pg.display.set_mode((800,600))
 
 pg.display.set_caption("snake game")
-class BodyPart :
-	def __init__(self,x,y):  
-		self.x=x
-		self.y=y
-		self.vel=5
-		self.lastX=x
-		self.lastY=y
-	
-	
-		
-	
-class Head (BodyPart): 
+
+
+#game components			
+class Head : 
 	def __init__(self):
-		BodyPart.__init__(self,randint(1,700),randint(1,500))
+		self.x=randint(1,700)
+		self.y=randint(1,500)
+		self.vel=32
 		self.direction = "right" 
 	
 	def move(self):
 		if self.direction == "right" :
-			self.lastX = self.x
 			if self.x <768 :
-				self.x+=self.vel
-				
+				self.x+=self.vel				
 			else:
 				self.x=0
 			
 		if self.direction == "left" :
-			self.lastX = self.x
 			if self.x >0 :
 				self.x-=self.vel
 			else:
 				self.x=768
 	
 		if self.direction == "top" :
-			self.lastY = self.y
 			if self.y > 0 :
 				self.y-=self.vel
 			else:
 				self.y = 568
 	
 		if self.direction == "bottom" :
-			self.lastY = self.y
 			if self.y < 568 :
 				self.y+=self.vel
 			else:
 				self.y=0
-	
-		
-class TailPart (BodyPart):
-	def __init__(self,x,y) :
-		BodyPart.__init__(self,x,y)
-		
-
-				 		
-		
+									 				
 class Snake: 
 	def __init__(self,head):
 		self.head=head
-		self.tail=[]
+		self.tail=[(self.head.x,self.head.y)]
 	
-	def growTail(self):
-		if len(self.tail) :
-			lastTailPart = self.tail[-1] 
-			self.tail.append(TailPart(lastTailPart.lastX,lastTailPart.lastY))
-		else :
-			self.tail.append(TailPart(self.head.lastX,self.head.lastY))
+	def growTail(self,headRect,foodRect,food):
+		if headRect.colliderect(foodRect):
+			if self.head.direction == "right" :
+				self.head.x+=32
+			elif self.head.direction == "left" :
+				self.head.x-=32
+			elif self.head.direction == "top" :
+				self.head.y-=32
+			else:
+				self.head.y+=32
+			self.tail.insert(0,(self.head.x,self.head.y))
+			food.gotEaten()
 	
-	def moveTail(self):
-		if self.head.direction in ["right","left"] :
-			self.tail[0].x=self.head.lastX
-			self.tail[0].y=self.head.lastY
-		else:
-			self.tail[0].x=self.head.lastX
-			self.tail[0].y=self.head.lastY-32
+	def move(self):
+		self.head.move()
+		self.tail.insert(0,(self.head.x,self.head.y))
+		del(self.tail[-1])				
+
+class Food: 
+	def __init__(self):
+		self.x = randint(1,700) 
+		self.y = randint(1,500)
+	
+	def gotEaten(self):
+		self.x = randint(1,700) 
+		self.y = randint(1,500)
 			
-	
+				
+
+
+#game loop
 head = Head()
 snake = Snake(head)
-
-
-
+food = Food() 
 running = True 
-i=0
+
 while running : 
-	pg.time.delay(100)
-	i+=1
-	screen.fill((0,0,0)) 
+	pg.time.delay(120)
+	screen.fill((128,128,128)) 
 	for event in pg.event.get() :
 		if event.type == pg.QUIT:
 			running = False
 	
-	#moving the head
+	#keys control
 	keys = pg.key.get_pressed()
 	if keys[pg.K_UP] :
-		snake.head.direction = "top"
+		if snake.head.direction != "bottom": 
+			snake.head.direction = "top"
 	
 	if keys[pg.K_DOWN] :
-		snake.head.direction = "bottom"
+		if snake.head.direction != "top":
+			snake.head.direction = "bottom"
 	
 	if keys[pg.K_LEFT] :
-		snake.head.direction = "left"
+		if snake.head.direction != "right":
+			snake.head.direction = "left"
 	
 	if keys[pg.K_RIGHT] :
-		snake.head.direction = "right" 
+		if snake.head.direction != "left":
+			snake.head.direction = "right" 
 
 	
-	snake.head.move()
-	pg.draw.rect(screen,(255,0,0),(snake.head.x,snake.head.y,32,32))
-	if  (i==5):
-		snake.growTail()			
-		pg.draw.rect(screen,(255,0,0),(snake.tail[0].x,snake.tail[0].y,32,32))
-	if i>5 :
-		snake.moveTail()
-		pg.draw.rect(screen,(0,255,0),(snake.tail[0].x,snake.tail[0].y,32,32))
+	snake.move()
+	#drawing the snake
+	for i in range(len(snake.tail)):
+		if i == 0 : 
+			headRect = pg.draw.rect(screen,(24,29,39),(snake.tail[i][0],snake.tail[i][1],32,32))
+		else:
+			if i%2 :
+		 		pg.draw.rect(screen,(37,77,50),(snake.tail[i][0],snake.tail[i][1],32,32))
+			else:
+				pg.draw.rect(screen,(24,29,39),(snake.tail[i][0],snake.tail[i][1],32,32))
+	
+	foodRect = pg.draw.rect(screen,(153,15,2),(food.x,food.y,16,16))
+	snake.growTail(headRect,foodRect,food)
 	pg.display.update()
 
 
